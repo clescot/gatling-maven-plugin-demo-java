3e9ae95cf927e299d8f4dc57ac8aebf263a8318d
Charles Lescot <charles@merovia.net>
2025-06-20 14:57:11 +0200

    add test

--- src/test/java/BuildCounterTest.java
+++ src/test/java/BuildCounterTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BuildCounterTest {
 
     @Test
-    public void testBuildCounter() {
+    public void testBuildCounterWithoutUnit() {
         // Given
         GatlingReporter gatlingReporter = new GatlingReporter();
         PrometheusRegistry prometheusRegistry = new PrometheusRegistry();
@@ -19,8 +19,26 @@ public class BuildCounterTest {
         Counter counter = gatlingReporter.buildCounter(prometheusRegistry, run, snakeCaseMetricName, counterValue+"");
 
         // Then
-        assertThat(counter.getPrometheusName()).isEqualTo("all_requests_number_of_requests");
+        assertThat(counter.getPrometheusName()).isEqualTo(snakeCaseMetricName);
         ;
         assertThat(counter.labelValues(run).getLongValue()).isEqualTo(counterValue);
+    }
+
+    @Test
+    public void testBuildCounterWithUnit() {
+        // Given
+        GatlingReporter gatlingReporter = new GatlingReporter();
+        PrometheusRegistry prometheusRegistry = new PrometheusRegistry();
+        String run = "basicsimulation_20250616094945181";
+        String snakeCaseMetricName = "all_requests_mean_response_time_ok";
+        long counterValue = 4235L;
+
+        // When
+        Counter counter = gatlingReporter.buildCounter(prometheusRegistry, run, snakeCaseMetricName, counterValue+"");
+
+        // Then
+        assertThat(counter.getPrometheusName()).isEqualTo(snakeCaseMetricName+"_seconds");
+        ;
+        assertThat(counter.labelValues(run).getLongValue()).isEqualTo(counterValue/1000);
     }
 }
